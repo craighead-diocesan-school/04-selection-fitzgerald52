6eb4ce37f0e92434d2b5bf81ddc221957a4ed2eb
fitzgerald52 <124209843+fitzgerald52@users.noreply.github.com>
2023-06-12 14:42:22 +1200

    i did the modify

--- code/2-conversation.js
+++ code/2-conversation.js
@@ -22,9 +22,24 @@ function conversation() {
   let answer = prompt()
 
   if (answer == 'yes' ) {
-   alert('That is cool so do I !')
-  } else {
-   alert ('Perhaps you like some other sport. ')
+   alert('That is cool so do I ! - do you play the sport ?')
+   let play =prompt()
+   if (play == 'yes' ){
+     alert('Its such a fun game to play !')
+   } else{
+     alert('Its such a fun game to whatch !')
+   }
+  } 
+  
+  else {
+   alert ('Perhaps you like some other sport. do you ? ')
+   let othersport = prompt()
+   if(othersport=='yes'){
+    alert('Sport is so much fun !')
+   } else{
+    alert('perhaps you have other hobbies')
+   }
+
 }
 alert('Goodbye')
 }
