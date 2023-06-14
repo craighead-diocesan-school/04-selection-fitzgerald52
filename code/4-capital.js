9513eea086fa537828574cf37a3dae28e176841f
fitzgerald52 <124209843+fitzgerald52@users.noreply.github.com>
2023-06-14 12:42:02 +1200

    the program says whether the answer to there question is correct or not

--- code/4-capital.js
+++ code/4-capital.js
@@ -4,4 +4,11 @@
 
 function capital() {
   // write your code here
+let name = prompt('what is your name ?')
+let answer = prompt('quick question, ' + name + ' what is the capital of France')
+if (answer == 'paris') {
+  alert("that's right " + name)
+} else {
+  alert('sorry that is incorrect')
+}
 }
