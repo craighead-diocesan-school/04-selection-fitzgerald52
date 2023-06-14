00ddee4d61ea53ae9c0a1996969534e3ccc9354e
fitzgerald52 <124209843+fitzgerald52@users.noreply.github.com>
2023-06-14 12:40:17 +1200

    i got the program to keep repeating until the user says stop

--- code/3-more-than-two.js
+++ code/3-more-than-two.js
@@ -4,4 +4,15 @@
 
 function moreThanTwo() {
   // write your code here
+  let startstop = ''
+  while (startstop != 'stop' ){
+    let answer = prompt('what number do you want to compare to 2')
+    answer = Number(answer)
+    if (answer > 2){
+      alert ( answer + ' is more than 2')
+    } else {
+      alert ( answer + ' is less than 2')
+    }
+    startstop = prompt('would you like to compare another number? if not say stop')
+  }
 }
