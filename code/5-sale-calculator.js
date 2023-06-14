a9467db70b5095beb59681e0eacbf58ef111b750
fitzgerald52 <124209843+fitzgerald52@users.noreply.github.com>
2023-06-14 12:32:45 +1200

    the program tells the user if they can afford an item on sale

--- code/5-sale-calculator.js
+++ code/5-sale-calculator.js
@@ -4,4 +4,16 @@
 
 function saleCalculator() {
   // write your code here
+  let money = prompt('How much money do you have to spend?')
+  let cost = prompt('How much money is the iteam normally?')
+  let discountpercentage = prompt('And what is the sales discount ?')
+  let discountdecimal = discountpercentage / 100
+  discount = cost * discountdecimal
+  let discountcost = 0
+  discountcost = cost - discount 
+  if ( money > discountcost){
+    alert('you have enough money')
+  } else{
+    alert('you do not have enough money. you have $' + money + ' to spend, but even with ' + discountpercentage + '% off, $' + cost + ' only comes down to $' + discountcost)
+  }
 }
